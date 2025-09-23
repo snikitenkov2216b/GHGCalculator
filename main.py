98990d94eae9d286e1960e570fe7d91b404f8198
snikitenkov2216b <snikitenkov200@gmail.com>
2025-09-23 23:07:12 +0700

    Обновил main.py для запуска GUI из ui.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-# main.py - Запуск приложения (базовая версия для теста)
-print("Привет! Это базовый запуск проекта GHGCalculator. Скоро добавим GUI.")
+# main.py - Запуск приложения
+from ttkthemes import ThemedTk
+from ui import GHGCalculator
+
+if __name__ == "__main__":
+    root = ThemedTk(theme="arc")  # Красивая тема
+    app = GHGCalculator(root)
+    root.mainloop()
